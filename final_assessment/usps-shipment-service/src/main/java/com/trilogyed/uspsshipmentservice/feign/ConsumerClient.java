6ac93ed505cd1d50ab4bf3a501b3be1d49196424
Shakib <nihan66@yahoo.com>
2019-10-16 11:48:24 -0400

    updated

--- final_assessment/usps-shipment-service/src/main/java/com/trilogyed/uspsshipmentservice/feign/ConsumerClient.java
+++ final_assessment/usps-shipment-service/src/main/java/com/trilogyed/uspsshipmentservice/feign/ConsumerClient.java
@@ -2,22 +2,20 @@ package com.trilogyed.uspsshipmentservice.feign;
 
 import com.trilogyed.uspsshipmentservice.model.Consumer;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@FeignClient(name = "consume-shipment-service")
+@FeignClient(name = "consumer-shipment-service")
+@RequestMapping("/consumer")
 public interface ConsumerClient {
 
 
-    @RequestMapping(value = "/consumer", method = RequestMethod.POST)
+   @PostMapping
     Consumer addConsumer(@RequestBody Consumer consumer);
 
-    @RequestMapping(value = "/consumer/{id}", method = RequestMethod.GET)
-    Consumer getConsumer(@PathVariable int trackingId);
+    @GetMapping("/{id}")
+    Consumer getConsumer(@PathVariable int id);
 
 
 }
